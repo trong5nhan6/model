263f52f884dfee26fbec63f5622d91d8635e34d0
Duc Duy <nhanhm.mm16@gmail.com>
2026-01-06 13:00:01 +0700

    test trm training func

--- tiny_recursive_model/trainer.py
+++ tiny_recursive_model/trainer.py
@@ -297,7 +297,6 @@ class Trainer(Module):
                     print("outputs.shape:", outputs.shape, "device:", outputs.device)
                     print("latents.shape:", latents.shape, "device:", latents.device)
                     print("active_images.shape:", active_images.shape, "device:", active_images.device)
-                    print("active_batch_indices.shape:", active_batch_indices.shape)
 
                     if not halt_mask.any():
                         continue
